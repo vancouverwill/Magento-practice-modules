9c18ba3d00f8eeb6c27d0af0b1e0c59ac9fadef0
Your Name <will_melbourne@hotmail.com>
2012-01-06 15:21:57 -0800

    added weblog module0.1

--- Weblog/controllers/IndexController.php
+++ Weblog/controllers/IndexController.php
@@ -2,9 +2,16 @@
 
 class Synergy_Weblog_IndexController extends Mage_Core_Controller_Front_Action {        
 
-	public function testModelAction() {
-        echo 'Setup!';
-    }
+public function testModelAction() {
+    $params = $this->getRequest()->getParams();
+    $blogpost = Mage::getModel('weblog/blogpost');
+    echo("Loading the blogpost with an ID of ".$params['id']);
+    $blogpost->load($params['id']);     
+    $data = $blogpost->getData();
+    var_dump($data);    
+}
+    
+    
 	
 	public function indexAction() {
 
